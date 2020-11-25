34ef15801f21485d0535d04cb06b9af391c4f835
mierenhoop <unconfigured@null.spigotmc.org>
2020-11-25 07:55:16 +0100

    Add houses, roads

--- source/logic/node.d
+++ source/logic/node.d
@@ -3,6 +3,23 @@ module node;
 import raylib;
 import board;
 
+struct Road
+{
+    int player = -1;
+}
+
+struct Building
+{
+    enum Type
+    {
+        House,
+        Town
+    }
+
+    int player = -1;
+    Type type;
+}
+
 class Node
 {
     enum Type
@@ -25,4 +42,23 @@ class Node
 
     uint[2] _indices;
     Board _board;
+
+    // Index starts at  
+    Node neighbour(uint index)
+    {
+        switch (index)
+        {
+            default:
+                throw new Error("Can't get neighbor at unknown index");
+        }
+    }
+
+    void addRoad(Road road, uint index)
+    {
+        
+    }
+
+private:
+    Road[6] _roads;
+    Building[6] _buildings;
 }
